9d5457c7951a85134662b1d14ae296cbe3542ef7
Awni Hannun <awni.hannun@gmail.com>
2020-07-10 12:44:00 -0700

    loss may work

--- transducer.py
+++ transducer.py
@@ -14,7 +14,7 @@ def thread_init():
 
 
 def make_chain_graph(sequence):
-    graph = gtn.Graph()
+    graph = gtn.Graph(False)
     graph.add_node(True)
     for i, s in range(sequence):
         graph.add_node(False, i == (len(sequence) - 1))
@@ -22,56 +22,92 @@ def make_chain_graph(sequence):
     return graph
 
 
+def make_lexicon_graph(word_pieces, graphemes_to_idx):
+    """
+    Constructs a graph which transudces letters to word pieces.
+    """
+    # TODO, consider direct construction as it could be more efficient
+    lex = []
+    for i, wp in enumerate(word_pieces):
+        graph = gtn.Graph(False)
+        graph.add_node(True)
+        for e, l in enumerate(wp):
+            if e == len(wp) - 1:
+                graph.add_node(False, True)
+                graph.add_arc(e, e + 1, graphemes_to_idx[l], i)
+            else:
+                graph.add_node()
+                graph.add_arc(e, e + 1, graphemes_to_idx[l], gtn.epsilon)
+        lex.append(graph)
+    return gtn.closure(gtn.sum(lex))
+
+
+def make_token_graph(token_list):
+    """
+    Constructs a graph with all the individual
+    token transition models.
+    """
+    # TODO, consider direct construction as it could be more efficient
+    tokens = []
+    for i, wp in enumerate(token_list):
+        # We can consume one or more consecutive
+        # word pieces for each emission:
+        # E.g. [ab, ab, ab] transduces to [ab]
+        graph = gtn.Graph(False)
+        graph.add_node(True)
+        graph.add_node(False, True)
+        graph.add_arc(0, 1, i, i)
+        graph.add_arc(1, 1, i, gtn.epsilon)
+        tokens.append(graph)
+    return gtn.closure(gtn.sum(tokens))
+
+
 class Transducer(torch.nn.Module):
 
-    def __init__(self, tokens, graphemes):
-        import pdb
-        pdb.set_trace()
+    def __init__(self, tokens, graphemes_to_idx, n_gram=0, blank=False):
         super(Transducer, self).__init__()
+        self.tokens = make_token_graph(tokens)
+        self.lexicon = make_lexicon_graph(tokens, graphemes_to_id)
+        if n_gram > 0:
+            raise NotImplementedError("Transition graphs not yet implemented.")
+        if blank:
+            raise NotImplementedError("Blank label not yet supported.")
+        self.transitions = None
 
     def forward(self, inputs, targets):
-        # TODO check if we have transitions before computing the log
-        log_probs = torch.nn.functional.log_softmax(inputs, dim=2)
-
-        log_probs = log_probs.permute(1, 0, 2).contiguous() # T x B X C ->  B x T x C
-        return TransducerLoss(log_probs, targets)
+        if transitions is None:
+            inputs = torch.nn.functional.log_softmax(inputs, dim=2)
+        inputs = inputs.permute(1, 0, 2).contiguous() # T x B X C ->  B x T x C
+        return TransducerLoss(inputs, targets, self.tokens, self.lexicon, self.transitions)
 
 
 class TransducerLossFunction(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, log_probs, targets):
-        grad_enabled = log_probs.requires_grad
-        is_cuda = log_probs.is_cuda
-        log_probs = log_probs.cpu()
-        B, T, C = list(log_probs.shape)
+    def forward(ctx, inputs, targets, tokens, lexicon, transitions=None):
+        B, T, C = inputs.shape
         loss = torch.zeros(B, dtype=torch.float)
-        if grad_enabled:
-            input_grad = torch.zeros(log_probs.shape)
 
         def process(b):
-            # create emission graph
+            # Create emissions graph:
             emissions = gtn.linear_graph(T, C, True)
-            emissions.set_weights(log_probs[b].flatten().tolist())
+            emissions.set_weights(inputs[b].cpu().data_ptr)
 
-            # create criterion graph
+            # Create alignment graph:
             target = make_chain_graph(target[b])
             alignments = gtn.project_input(gtn.remove(gtn.compose(tokens, target)))
             num = gtn.forward_score(gtn.intersect(emissions, alignments))
-            loss[b] = fwd_graph.item() * scale
-
-            if grad_enabled:
-                gtn.backward(fwd_graph, False)
-                grad = emissions.grad().weights()
-                input_grad[b] = torch.Tensor(grad).view(1, T, C)
-                input_grad[b] *= scale
+            if transitions is not None:
+                denom = gtn.forward_score(gtn.intersect(emissions, transitions))
+                loss[b] = gtn.subtract(denom, num).item()
+            else:
+                loss[b] = gtn.negate(num).item()
 
         executor = ThreadPoolExecutor(max_workers=B, initializer=thread_init)
         futures = [executor.submit(process, b) for b in range(B)]
         for f in futures:
             f.result()
-        ctx.constant = input_grad.cuda() / B if is_cuda else input_grad / B
-        return torch.mean(loss.cuda() if is_cuda else loss)
+        return torch.mean(loss.cuda() if inputs.is_cuda() else loss)
 
     @staticmethod
     def backward(ctx, grad_output):
