43a6fe5a45d1ff6743b4b5706b7fe265ca5abc9f
Awni Hannun <awni@fb.com>
2020-07-15 16:26:26 -0400

    ctc test in transducer

--- tests/transducer_test.py
+++ tests/transducer_test.py
@@ -15,28 +15,25 @@ class TestTransducer(unittest.TestCase):
     def test_fwd_trivial(self):
         T = 3
         N = 2
-        emissions = torch.FloatTensor([1.0, 0.0, 0.0, 1.0, 1.0,
-                                       0.0]).view(T, 1, N)
+        emissions = torch.FloatTensor([1.0, 0.0, 0.0, 1.0, 1.0, 0.0]).view(T, 1, N)
         log_probs = torch.log(emissions)
 
         # Check without blank:
         labels = [[0, 1, 0]]
-        transducer = Transducer(
-            tokens=["a", "b"], graphemes_to_idx={"a": 0, "b": 1})
+        transducer = Transducer(tokens=["a", "b"], graphemes_to_idx={"a": 0, "b": 1})
         self.assertAlmostEqual(transducer(log_probs, labels).item(), 0.0)
 
         # Check with blank:
         labels = [[0, 0]]
-        transducer = Transducer(
-            tokens=["a"], graphemes_to_idx={"a": 0}, blank=True)
+        transducer = Transducer(tokens=["a"], graphemes_to_idx={"a": 0}, blank=True)
         self.assertAlmostEqual(transducer(log_probs, labels).item(), 0.0)
 
         # Check with repeats not allowed:
         labels = [[0, 0]]
         transducer = Transducer(
-            tokens=["a"], graphemes_to_idx={"a": 0}, blank=True, allow_repeats=False)
+            tokens=["a"], graphemes_to_idx={"a": 0}, blank=True, allow_repeats=False
+        )
         self.assertAlmostEqual(transducer(log_probs, labels).item(), 0.0)
-
 
     def test_fwd(self):
         T = 3
@@ -47,103 +44,93 @@ class TestTransducer(unittest.TestCase):
         log_probs = torch.nn.functional.log_softmax(torch.log(emissions), 2)
         transducer = Transducer(
             tokens=["a", "b", "c"],
-            graphemes_to_idx={"a" : 0, "b" : 1, "c": 2},
-            blank=True)
+            graphemes_to_idx={"a": 0, "b": 1, "c": 2},
+            blank=True,
+        )
         fwd = transducer(log_probs, labels)
         self.assertAlmostEqual(fwd.item(), -math.log(0.25 * 0.25 * 0.25 * 5))
 
-    def test_fwd_bwd(self):
+    def test_ctc(self):
         T = 5
         N = 6
+
+        # Test 1
         labels = [[0, 1, 2, 1, 0]]
-        emissions = torch.tensor(
-            (
-                0.633766,
-                0.221185,
-                0.0917319,
-                0.0129757,
-                0.0142857,
-                0.0260553,
-                0.111121,
-                0.588392,
-                0.278779,
-                0.0055756,
-                0.00569609,
-                0.010436,
-                0.0357786,
-                0.633813,
-                0.321418,
-                0.00249248,
-                0.00272882,
-                0.0037688,
-                0.0663296,
-                0.643849,
-                0.280111,
-                0.00283995,
-                0.0035545,
-                0.00331533,
-                0.458235,
-                0.396634,
-                0.123377,
-                0.00648837,
-                0.00903441,
-                0.00623107,
+        # fmt: off
+        emissions = torch.tensor((
+            0.633766,  0.221185, 0.0917319, 0.0129757,  0.0142857,  0.0260553,
+            0.111121,  0.588392, 0.278779,  0.0055756,  0.00569609, 0.010436,
+            0.0357786, 0.633813, 0.321418,  0.00249248, 0.00272882, 0.0037688,
+            0.0663296, 0.643849, 0.280111,  0.00283995, 0.0035545,  0.00331533,
+            0.458235,  0.396634, 0.123377,  0.00648837, 0.00903441, 0.00623107,
             ),
             requires_grad=True,
         )
+        # fmt: on
         log_emissions = torch.log(emissions.view(T, 1, N))
-        log_probs = torch.nn.functional.log_softmax(log_emissions, 2)
         log_emissions.retain_grad()
         transducer = Transducer(
             tokens=["a", "b", "c", "d", "e"],
-            graphemes_to_idx={"a" : 0, "b" : 1, "c": 2, "d":3, "e": 4},
-            blank=True)
+            graphemes_to_idx={"a": 0, "b": 1, "c": 2, "d": 3, "e": 4},
+            blank=True,
+        )
 
-        loss = transducer(log_probs, labels)
+        loss = transducer(log_emissions, labels)
         self.assertAlmostEqual(loss.item(), 3.34211, places=4)
         loss.backward()
+        # fmt: off
         expected_grad = torch.tensor((
-            -0.366234,
-            0.221185,
-            0.0917319,
-            0.0129757,
-            0.0142857,
-            0.0260553,
-            0.111121,
-            -0.411608,
-            0.278779,
-            0.0055756,
-            0.00569609,
-            0.010436,
-            0.0357786,
-            0.633813,
-            -0.678582,
-            0.00249248,
-            0.00272882,
-            0.0037688,
-            0.0663296,
-            -0.356151,
-            0.280111,
-            0.00283995,
-            0.0035545,
-            0.00331533,
-            -0.541765,
-            0.396634,
-            0.123377,
-            0.00648837,
-            0.00903441,
-            0.00623107,
+            -0.366234, 0.221185,  0.0917319, 0.0129757,  0.0142857,  0.0260553,
+            0.111121,  -0.411608, 0.278779,  0.0055756,  0.00569609, 0.010436,
+            0.0357786, 0.633813,  -0.678582, 0.00249248, 0.00272882, 0.0037688,
+            0.0663296, -0.356151, 0.280111,  0.00283995, 0.0035545,  0.00331533,
+            -0.541765, 0.396634,  0.123377,  0.00648837, 0.00903441, 0.00623107,
         )).view(T, 1, N)
+        # fmt: on
         self.assertTrue(log_emissions.grad.allclose(expected_grad))
 
+        # Test 2
+        labels = [[0, 1, 1, 0]]
+        # fmt: off
+        emissions = torch.tensor((
+            0.30176,  0.28562,  0.0831517, 0.0862751, 0.0816851, 0.161508,
+            0.24082,  0.397533, 0.0557226, 0.0546814, 0.0557528, 0.19549,
+            0.230246, 0.450868, 0.0389607, 0.038309,  0.0391602, 0.202456,
+            0.280884, 0.429522, 0.0326593, 0.0339046, 0.0326856, 0.190345,
+            0.423286, 0.315517, 0.0338439, 0.0393744, 0.0339315, 0.154046,
+            ),
+            requires_grad=True,
+        )
+        # fmt: on
+        log_emissions = torch.log(emissions.view(T, 1, N))
+        log_emissions.retain_grad()
+        transducer = Transducer(
+            tokens=["a", "b", "c", "d", "e"],
+            graphemes_to_idx={"a": 0, "b": 1, "c": 2, "d": 3, "e": 4},
+            blank=True,
+            allow_repeats=False,
+        )
+        loss = transducer(log_emissions, labels)
+        self.assertAlmostEqual(loss.item(), 5.42262, places=4)
+        loss.backward()
+
+        # fmt: off
+        expected_grad = torch.tensor((
+            -0.69824,  0.28562,   0.0831517, 0.0862751, 0.0816851, 0.161508,
+            0.24082,   -0.602467, 0.0557226, 0.0546814, 0.0557528, 0.19549,
+            0.230246,  0.450868,  0.0389607, 0.038309,  0.0391602, -0.797544,
+            0.280884,  -0.570478, 0.0326593, 0.0339046, 0.0326856, 0.190345,
+            -0.576714, 0.315517,  0.0338439, 0.0393744, 0.0339315, 0.154046,
+        )).view(T, 1, N)
+        # fmt: on
+        self.assertTrue(log_emissions.grad.allclose(expected_grad))
 
     def test_simple_decomposition(self):
         T = 5
         tokens = ["a", "b", "ab", "ba", "aba"]
         scores = torch.randn((T, 1, len(tokens)), requires_grad=True)
         labels = [[0, 1, 0]]
-        transducer = Transducer(
-            tokens=tokens, graphemes_to_idx={"a": 0, "b": 1})
+        transducer = Transducer(tokens=tokens, graphemes_to_idx={"a": 0, "b": 1})
 
         # Hand construct the alignment graph with all of the decompositions
         alignments = gtn.Graph(False)
@@ -181,7 +168,8 @@ class TestTransducer(unittest.TestCase):
         emissions.set_weights(scores.flatten().tolist())
         expected_loss = gtn.subtract(
             gtn.forward_score(emissions),
-            gtn.forward_score(gtn.intersect(emissions, alignments)))
+            gtn.forward_score(gtn.intersect(emissions, alignments)),
+        )
 
         loss = transducer(scores, labels)
         self.assertAlmostEqual(loss.item(), expected_loss.item(), 5)
@@ -190,8 +178,9 @@ class TestTransducer(unittest.TestCase):
 
         expected_grad = torch.tensor(emissions.grad().weights())
         expected_grad = expected_grad.view((T, 1, len(tokens)))
-        self.assertTrue(torch.allclose(
-            scores.grad, expected_grad, rtol=1e-4, atol=1e-5))
+        self.assertTrue(
+            torch.allclose(scores.grad, expected_grad, rtol=1e-4, atol=1e-5)
+        )
 
 
 if __name__ == "__main__":
